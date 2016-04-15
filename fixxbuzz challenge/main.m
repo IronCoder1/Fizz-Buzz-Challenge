485786dbb0515ab0f1d32e3ee6757027726f6e60
Anthony Olu <b.ioanth1000@gmail.com>
2016-04-15 23:52:34 +0100

    fizzbuzz challenge

---
+++ fixxbuzz challenge/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  fixxbuzz challenge
+//
+//  Created by MBPinTheAir on 01/03/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+//        NSLog(@"Hello, World!");
+        int fizz = 3;
+        int buzz = 5
+        
+    }
+    return 0;
+}
